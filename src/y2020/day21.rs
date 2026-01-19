957318fbd0b03d5cee40eb4ef85ba38b331f942f
vango <viktorlilienberg@gmail.com>
2026-01-19 14:07:43 +0100

    2020 21, constraint propagation,

--- src/y2020/day21.rs
+++ src/y2020/day21.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use crate::Solution;
 
@@ -20,18 +20,21 @@ impl Solution for Day21 {
     }
 }
 
-fn solve_part_a(input: &str) -> i32 {
-    //allergens -> ingredients
+fn solve_part_a(input: &str) -> u32 {
+    // allergens -> possible ingredients
     let mut map: HashMap<String, Vec<String>> = HashMap::new();
+    let mut ingredient_apperance: HashMap<String, u32> = HashMap::new();
 
     for l in input.lines() {
         let mut parts = l.split("(contains");
+
         let ingredients: Vec<String> = parts
             .next()
             .unwrap()
             .split_whitespace()
             .map(|s| s.to_string())
             .collect();
+
         let allergens: Option<Vec<String>> = parts.next().map(|s| {
             s.trim_end_matches(')')
                 .split(',')
@@ -39,28 +42,78 @@ fn solve_part_a(input: &str) -> i32 {
                 .collect()
         });
 
-        match allergens {
-            Some(list) => {
-                // nu vill vi slå upp på varje allergen
-                for alg in list {
-                    map.entry(alg)
-                        .and_modify(|e| {
-                            e.retain(|ingr| ingredients.contains(ingr));
-                        })
-                        .or_insert(ingredients.iter().map(|s| s.to_string()).collect());
-                }
+        if let Some(list) = allergens {
+            for alg in list {
+                map.entry(alg)
+                    .and_modify(|e| {
+                        e.retain(|ingr| ingredients.contains(ingr));
+                    })
+                    .or_insert(ingredients.to_vec());
             }
-            None => continue,
+        }
+
+        //insert ingredients to set
+        for i in ingredients {
+            ingredient_apperance
+                .entry(i)
+                .and_modify(|times| *times += 1)
+                .or_insert(1);
         }
     }
 
-    // now map will map each allergen to a ingredient,
-    // so ingredients not apart of map is free,
-    //
-    // make a hashset , and add udirng  init phase, iter over set and se if apart over values from
-    // map
+    // constraint propagation
 
-    0
+    loop {
+        let mut changed = false;
+
+        let singles = map
+            .iter()
+            .filter_map(|(a, v)| {
+                if v.len() == 1 {
+                    Some((a.clone(), v[0].clone()))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<(String, String)>>();
+
+        let keys: Vec<String> = map.keys().cloned().collect();
+        for (allergen, ingred) in singles {
+            //remove value from all where allergens != key
+            for key in &keys {
+                if key == &allergen {
+                    continue;
+                }
+
+                map.entry(key.clone()).and_modify(|list| {
+                    let before = list.len();
+                    list.retain(|i| i != &ingred);
+                    if before != list.len() {
+                        changed = true;
+                    }
+                });
+            }
+        }
+
+        if !changed {
+            break;
+        }
+    }
+
+    // Each allergen will map to 1 ingredient, lets pull them out into an array!
+    let allergens = map.values().map(|v| v[0].clone()).collect::<Vec<String>>();
+
+    // go through each allergen in set, count each that allergens does not contain
+    let mut sum = 0;
+    for (i, count) in ingredient_apperance.iter() {
+        if !allergens.contains(i) {
+            sum += count;
+        }
+    }
+
+    println!("{:?}", map);
+
+    sum
 }
 
 fn solve_part_b(_input: &str) -> i32 {
